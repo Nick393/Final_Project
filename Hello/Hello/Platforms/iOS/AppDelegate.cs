3727b4559b8887379ac41667242ec8b5071ef834
Nick393 <82907413+Nick393@users.noreply.github.com>
2023-04-18 08:13:43 -0500

    removed other stuff

--- Hello/Hello/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Hello;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
